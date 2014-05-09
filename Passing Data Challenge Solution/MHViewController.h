a1c2f9ffbc3c9abd78bc48046ef1d778b6845901
Mike Hoover <mikehoover@sc.rr.com>
2014-05-09 09:55:26 -0400

    Initial Commit

---
+++ Passing Data Challenge Solution/MHViewController.h
@@ -0,0 +1,13 @@
+//
+//  MHViewController.h
+//  Passing Data Challenge Solution
+//
+//  Created by Mike Hoover on 5/9/14.
+//  Copyright (c) 2014 Mike Hoover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHViewController : UIViewController
+
+@end
---
+++ Passing Data Challenge Solution/MHViewController.m
@@ -0,0 +1,29 @@
+//
+//  MHViewController.m
+//  Passing Data Challenge Solution
+//
+//  Created by Mike Hoover on 5/9/14.
+//  Copyright (c) 2014 Mike Hoover. All rights reserved.
+//
+
+#import "MHViewController.h"
+
+@interface MHViewController ()
+
+@end
+
+@implementation MHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
